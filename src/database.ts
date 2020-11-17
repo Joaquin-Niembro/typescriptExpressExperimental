74265bfe81a06f1b48f1206ee24042d39dd2b76d
joaquin <joaquinniembro@gmail.com>
2020-11-17 16:17:44 -0600

    v3

--- src/database.ts
+++ src/database.ts
@@ -1,20 +1,22 @@
-import {createConnection} from 'typeorm'
-import {Task, Person} from './entity/'
-export const con = async ():Promise<void> =>{
-    const connection = await createConnection({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-		password: 'password',
-		database: 'typeorm',
+import { createConnection } from 'typeorm';
+import { Task, Person } from './entity/';
+import {config} from 'dotenv'
+config()
+export const con = async (): Promise<void> => {
+	const connection = await createConnection({
+		type: 'postgres',
+		host: process.env.host,
+		port: 5432,
+		username: process.env.db_user,
+		password: process.env.db_password,
+		database: process.env.db,
 		synchronize: true,
 		logging: false,
-		entities: [Task, Person]
-    })
-    if(connection){
-        console.log('db connected')
-    }else{
-        console.log('db connection failed')
-    }
-}
+		entities: [Task, Person],
+	});
+	if (connection) {
+		console.log('db connected');
+	} else {
+		console.log('db connection failed');
+	}
+};
